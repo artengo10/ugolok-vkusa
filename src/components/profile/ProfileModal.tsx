98b4407a44777f22748b8f854c6511fc4d4c7548
artengo10 <artemklimanov64@gmail.com>
2026-05-22 16:10:11 +0300

    fix: make app link clickable with App Store coming soon alert

--- src/components/profile/ProfileModal.tsx
+++ src/components/profile/ProfileModal.tsx
@@ -63,7 +63,8 @@ export default function ProfileModal({ open, onClose }: Props) {
             </span>
           </div>
           <p className="text-xs text-amber-700 dark:text-amber-400 pl-6">
-            Копи и трать баллы в нашем приложении
+            Копи и трать баллы в нашем{' '}
+            <a href="#" className="underline font-semibold hover:opacity-80" onClick={(e) => { e.preventDefault(); alert('Приложение скоро появится в App Store!'); }}>приложении</a>
           </p>
         </div>
 
